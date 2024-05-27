9ff9b97e81f4fba3a30348674cd08561d60ef23f
jgl3hb <lewis.jeffreyg@gmail.com>
2024-05-26 21:06:08 -0600

    searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+// src/components/SearchBar.js
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search All Chats by Topic" />
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
